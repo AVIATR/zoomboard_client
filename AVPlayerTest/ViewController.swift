b96ced02844c13e17b3c17d168c57cf98b788671
Giovanni Fusco <giofusco@ski.org>
2019-01-31 14:08:48 -0800

    first commit

--- AVPlayerTest/ViewController.swift
+++ AVPlayerTest/ViewController.swift
@@ -7,12 +7,47 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func playVideo(_ sender: Any) {
+        
+        guard let url = URL(string: "https://github.com/versluis/Movie-Player/blob/master/Movie%20Player/video.mov?raw=true") else {
+            return
+        }
+        
+        // Create an AVPlayer, passing it the HTTP Live Streaming URL.
+        let player = AVPlayer(url: url)
+        
+        // Create a new AVPlayerViewController and pass it a reference to the player.
+        let controller = AVPlayerViewController()
+        controller.player = player
+        
+        // Modally present the player and call the player's play() method when complete.
+        present(controller, animated: true) {
+            player.play()
+        }
+    }
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        // Getting the shared instance of the audio session and setting audio to continue playback when the screen is locked.
+        let audioSession = AVAudioSession.sharedInstance()
+        do {
+            try audioSession.setCategory(AVAudioSession.Category.playback, mode: AVAudioSession.Mode.default, policy: AVAudioSession.RouteSharingPolicy.default, options: AVAudioSession.CategoryOptions.allowBluetooth)
+        }
+        catch {
+            print("Setting category to AVAudioSessionCategoryPlayback failed.")
+        }
+        return true
     }
 
 
